62d276c603061d9a39efa6fa8c1a000061394fc1
Richie-Adoteye <richardadoteye345@gmail.com>
2026-05-31 22:03:15 +0000

    Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+#File: inittials.py
+#Description: Print out my initials
+#Assignment Number: 2
+#
+#Name: Richard Adoteye
+#STUDENT ID: 2425400519
+#Email: richardadoteye345@gmail.com
+#Grader: Augustus
+#
+#On my honor, Richard Adoteye, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #Print out my initials
+    print()
+    print("...RAD")
+    print()
 
 
+    print("...RRRRRRRRRRRR........AAAAAAAAAAAA........DDDDD............")
+    print("...RR........RR........AA........AA........DD...DDD.........")
+    print("...RR........RR........AA........AA........DD....DDD........")
+    print("...RRRRRRRRRRRR........AAAAAAAAAAAA........DD......DDD......")
+    print("...RR..RR..............AA........AA........DD.......DDD.....")
+    print("...RR...RR.............AA........AA........DD.......DDD.....")
+    print("...RR....RR............AA........AA........DD......DDD......")
+    print("...RR.....RR...........AA........AA........DD....DDD........")
+    print("...RR......RR.....**...AA........AA...**...DD...DDD.......**")
+    print("...RR.......RRR...**...AA........AA...**...DDDDD..........**")
+    print()
 
 main()
